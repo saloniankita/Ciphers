b8a99a3e4b395c9e4ec3f479b1aa696fb5b55221
Ankita Saloni <56873389+saloniankita23@users.noreply.github.com>
2020-03-29 01:21:28 +0530

    Created Caeser-Cipher.cpp

---
+++ Caeser-Cipher/Caeser-Cipher.cpp
@@ -0,0 +1,31 @@
+    #include <iostream>
+    #include <string>
+
+    using namespace std;
+
+    int main() 
+   {
+	string cipher{};
+	cout<<" ENTER  THE  PLAIN  TEXT : "<<endl;
+	getline(cin, cipher);
+
+
+        cout<<" DECRYPTED  TEXT : ";
+	 for (auto c: cipher)
+		if(c != ' ' && c != 'z') 
+		{
+			char d = c + 3;
+		 	cout<<d;
+		  }
+		
+		else if(c == 'z')
+		{
+			c = 'A';
+			cout<<c;
+		  }
+		
+		 else
+			cout<<c;
+
+	return 0;
+     }
